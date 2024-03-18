d5e95c91579ba83179d57d10c21a98ef7eb17dc4
Bharath-kumar <bharath.kumar@insurancedekho.com>
2024-03-18 09:42:11 +0530

    cors fix

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow requests from all origins
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
