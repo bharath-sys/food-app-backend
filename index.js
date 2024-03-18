b22a71f6559aa1bde260c0b62cde2fddcb2dd913
Bharath-kumar <bharath.kumar@insurancedekho.com>
2024-03-18 09:47:39 +0530

    added methods to cors

--- index.js
+++ index.js
@@ -19,9 +19,11 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // Allow requests from all origins
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specified methods
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // Include 'Authorization'
     next();
 });
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
